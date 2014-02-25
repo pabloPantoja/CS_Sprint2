b377093a4947a262ed9118d76c2fc88c85245c15
Pablo Pantoja Diaz <pablopantoja1985@gmail.com>
2014-02-25 17:07:44 +0100

    version 25/02/14, correcion de codigo

--- src/main/java/cs_sprint2/PersonaAppCS.java
+++ src/main/java/cs_sprint2/PersonaAppCS.java
@@ -34,8 +34,6 @@ public class PersonaAppCS implements Serializable {
     protected double peso5;
     protected double altura5;
     
-    protected double peso6;
-    protected double altura6;
 
     public String getDni() {
         return dni;
