8378fb226ff733ce352b9d8f43bb8d7ccfe09029
Pablo Pantoja Diaz <pablopantoja1985@gmail.com>
2014-02-25 16:28:13 +0100

    cosas raras para PMD

--- src/main/java/cs_sprint2/App.java
+++ src/main/java/cs_sprint2/App.java
@@ -4,10 +4,8 @@ import java.util.*;
 
 
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ){
+        System.out.println("Hello World!");
     }
 }
